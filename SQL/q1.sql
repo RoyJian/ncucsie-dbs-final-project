a41af393282bfacadc45b09f767331318338ee82
RoyJian <roy881124@icloud.com>
2023-06-11 21:01:00 +0800

    ♻️ Revise Data Schema

--- SQL/q1.sql
+++ SQL/q1.sql
@@ -1,7 +1,17 @@
-UPDATE Course
-SET Location_ID = (SELECT Location_ID FROM Course_Location WHERE Room_Name = 'K210')
-WHERE Course_No = 'A0001' AND Course_name ='微積分';
+UPDATE Section_Location_Time
+SET Room_Name = 'K210'
+WHERE Section_No = 'A0001' AND Semester = 1112;
 
-SELECT course_no As '課號', course_name As '課名', Room_Name As '教室', Room_Build As '系館'
-FROM Course JOIN Course_Location ON Course.Location_ID = Course_Location.Location_ID
-WHERE Course.Course_name = '微積分'
+SELECT 
+    Section_Location_Time.Section_No  As '課號', 
+    Course_Name As '課名',
+    Section_Location_Time.Week As '星期',
+    Section_Location_Time.Period As '節次',
+    Room_Name As '教室',
+    Room_Building As '系館'
+FROM 
+    Section 
+    JOIN Section_Location_Time ON Section."Section_No" = Section_Location_Time."Section_No"
+	JOIN Course ON Section."Course_ID" = Course."Course_ID"
+WHERE Section_Location_Time.Section_No = 'A0001' AND Section_Location_Time.Semester= 1112;
+
--- SQL/q2.sql
+++ SQL/q2.sql
@@ -1,4 +1,11 @@
-SELECT Student.Student_name As "姓名",  Student_Dept.Dept_Name As "科系", Student.Student_Grade As "年級", Student.Student_Class As "班級"
-FROM Student, Course, Student_Course_Record, Student_Dept
-WHERE Student_Course_Record.Student_ID = Student.Student_ID AND Student.Student_Dept =Student_Dept.Dept_ID
-AND  Student_Course_Record.Course_No = Course.Course_No AND Course.Course_name = '計算機概論'  AND Semester = 1112;
+SELECT 
+	Student.Student_name As "姓名",  Student_Dept.Dept_Name As "科系", 
+	Student.Student_Grade As "年級", Student.Student_Class As "班級"
+FROM 
+    Student, Course, Student_Course_Record, Student_Dept,Section
+WHERE Student_Course_Record.Student_ID = Student.Student_ID 
+AND Student.Student_Dept=Student_Dept.Dept_ID
+AND  Student_Course_Record.Section_No = Section.Section_No
+AND Section.Course_ID = Course.Course_ID
+AND Section.Section_No='A0002'
+AND Student_Course_Record.Select_Result != 0 AND Section.Semester = 1112;
